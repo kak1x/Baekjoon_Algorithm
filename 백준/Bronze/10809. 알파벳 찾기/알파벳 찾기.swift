799394c7b21ee738a9726228fc891341830ab4c8
kaki <51234397+kak1x@users.noreply.github.com>
2022-12-01 21:28:22 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 8 ms, Memory: 69096 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.swift
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.swift
@@ -1,14 +1,9 @@
-var str = readLine()!.map { String($0) }
-
-var engArr: [String] = []
+let str = Array(readLine()!)
 
 for i in Int(UnicodeScalar("a").value)...Int(UnicodeScalar("z").value) {
-    engArr.append(String(UnicodeScalar(i)!))
-}
-
-for i in engArr {
-    if str.contains(i) {
-        print(str.firstIndex(of: i)!, terminator: " ")
+    let char = Character(UnicodeScalar(i)!)
+    if str.contains(char) {
+        print(str.firstIndex(of: char)!, terminator: " ")
     } else {
         print(-1, terminator: " ")
     }
