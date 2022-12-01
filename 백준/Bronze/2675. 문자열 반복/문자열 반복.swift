3cd9837ead0136f00c0bba2e775070246b465da3
kaki <51234397+kak1x@users.noreply.github.com>
2022-12-02 01:46:53 +0900

    [Bronze II] Title: 문자열 반복, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.swift
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.swift
@@ -3,7 +3,7 @@ let repeatNum = Int(readLine()!)!
 for _ in 1...repeatNum{
     let arr = readLine()!.split(separator: " ")
     let repeatChar = Int(arr[0])!
-    let strArr = Array(arr[1])
+    let strArr = arr[1]
     
     for i in strArr {
         for _ in 1...repeatChar {
