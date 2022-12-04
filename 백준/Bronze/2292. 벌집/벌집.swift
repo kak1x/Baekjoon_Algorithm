3720d2fb7215002146bb5ba8712c01aa29460441
kaki <ycg02109@naver.com>
2022-12-04 17:59:48 +0900

    [Bronze II] Title: 벌집, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.swift
@@ -0,0 +1,15 @@
+let n = Int(readLine()!)!
+var sum = 1
+var count = 1
+for i in 1...n {
+    if n == 1 {
+        print(count)
+        break
+    }
+    if n <= sum {
+        print(count)
+        break
+    }
+    sum += 6 * i
+    count += 1
+}
