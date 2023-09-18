b35442d7f0b2a5f996d93922311497d0ffe3ace8
kaki <ycg02109@naver.com>
2023-09-18 10:49:14 +0900

    [Bronze V] Title: 시험 성적, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.swift
@@ -0,0 +1,16 @@
+let score = Int(readLine()!)!
+
+switch(score / 10) {
+case 10:
+    fallthrough
+case 9:
+    print("A")
+case 8:
+    print("B")
+case 7:
+    print("C")
+case 6:
+    print("D")
+default:
+    print("F")
+}
